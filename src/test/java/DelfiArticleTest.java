cc2b0f2cad6ab696f419d865f413ae9ac29b7047
Katjuxa <katjenok@gmail.com>
2019-10-13 14:07:46 +0300

    Updated

--- src/test/java/DelfiArticleTest.java
+++ src/test/java/DelfiArticleTest.java
@@ -1,28 +1,37 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class DelfiArticleTest {
 
     private final By HOME_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'text-size-22')]");
     private final By HOME_PAGE_COMMENTS = By.xpath(".//a[@class='comment-count text-red-ribbon']");
-    private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'text-size-md-30'")]);
-    private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'text-size-19')"]);
+    private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'text-size-md-30')]");
+    private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'text-size-19')]");
     private final By COMMENT_PAGE_TITLE = By.xpath(".//h1[@class = 'article-title']/a");
-    private final By ARTICLE = By.xpath(".//span[@class = 'text-size-22 d-block']")
-
+    private final By ARTICLE = By.xpath(".//span[@class = 'text-size-22 d-block']");
+    private final By COMMENT_PAGE_COMMENT = By.xpath(".//span [@class = 'type-cnt']");
+    private static final Logger LOGGER = LogManager.getLogger(DelfiArticleTest.class);
 
 
     @Test
     public void titleAnsCommentsTest() {
 
+        LOGGER.info("Open browser");
         //Open browser
-        System.setProperty("webdriver.chrome.driver", "C:/Users/HomePC/Desktop/Katja/chromedriver_win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/QA2/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+
+        LOGGER.info("Open Delfi Home");
         //Open Delfi Home page
         driver.get("http://rus.delfi.lv");
 
@@ -30,14 +39,12 @@ public class DelfiArticleTest {
         WebElement article = driver.findElements(ARTICLE).get(0);
 
         //Find 1st article title
-//        WebElement homePageTitle = driver.findElement(HOME_PAGE_TITLE);
         WebElement homePageTitle = article.findElement(HOME_PAGE_TITLE);
 
         //Save to String
-        String titleToCompare = homePageTitle.getText();
+        String titleToCompare = homePageTitle.getText().trim();
 
         //Find comments count
-//        WebElement homePageComments = driver.findElement(HOME_PAGE_COMMENTS);
         Integer commentsToCompare = 0;
 
         if (!article.findElements(HOME_PAGE_COMMENTS).isEmpty()) { //! menjaet znachenie na protivopolozhnoe
@@ -52,7 +59,7 @@ public class DelfiArticleTest {
         homePageTitle.click();
 
         //Find article title
-        String apTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
+        String apTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText().trim();
 
         //Check title
         Assertions.assertEquals(titleToCompare, apTitle, "Wrong title on Article page");
@@ -67,15 +74,50 @@ public class DelfiArticleTest {
         driver.findElement(ARTICLE_PAGE_COMMENTS).click();
 
         //Find title
-        String cpTitle = driver.findElement(COMMENT_PAGE_TITLE).getText();
+        String cpTitle = driver.findElement(COMMENT_PAGE_TITLE).getText().trim();
 
         //Check title
+        LOGGER.info("Check title on comment page");
         Assertions.assertEquals(titleToCompare, cpTitle, "Wrong title on comment page");
 
-        //Get comment count
+        //Get comment  count
+        List<WebElement> cpComments = driver.findElements(COMMENT_PAGE_COMMENT);
 
+//        String anonim  = driver.findElements(COMMENT_PAGE_COMMENT).get(0).getText();
+//        String reg  = driver.findElements(COMMENT_PAGE_COMMENT).get(1).getText();
+
+        String anonim = cpComments.get(0).getText();
+        String anonim1 = anonim.substring(1, anonim.length() -1);
+        Integer anon_com = Integer.valueOf(anonim1);
+        String reg = cpComments.get(1).getText();
+        String reg1 = reg.substring(1, reg.length() -1);
+        Integer reg_com = Integer.valueOf(reg1);
+
+        //Result of comment count on comment page
+        Integer com_on_cp = anon_com + reg_com;
 
         //Check comment count
+        Assertions.assertEquals(commentsToCompare, com_on_cp, "Wrong comment count on comment page");
+
         //Close browser
+        driver.close();
+        System.out.println(anonim1);
+
+
     }
-}
+
+//        String qwer = cpComments.get(0).g
+
+//        WebElement ee = driver.findElements(COMMENT_PAGE_COMMENT).get(0);
+
+        //        WebElement suka = cpComments.get(0);
+//        String aaa = suka.getText()
+//        for (int i = 0; i < cpComments.size(); i++) {
+//            WebElement anonim = toString();
+//        }
+
+
+
+//        driver.close();
+    }
+//}
